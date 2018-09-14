ed549dc0f2c97f8d9495ca7ca34f09b929b26f79
njbroom <njbroom@csl4wk29.win2k.cs.ust.hk>
2018-09-14 09:51:38 +0800

    Second commit.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,8 +4,11 @@
 public class Library {
 	
 	public static void main(String args[]) {
-		System.out.println("This is the main method.");
-	}
+		Library lib = new Library();
+		if(lib.someLibraryMethod()) {
+			System.out.println("When there is a fire, commit and push.");
+		}
+}
 	
     public boolean someLibraryMethod() {
         return true;
